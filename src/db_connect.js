3d8c7f4829cb90cb2eed769af1e08f626da7478a
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2021-03-01 20:37:47 -0500

    Database Connection
    File contains how we can connect to local database using the pg library.
    Contains instruction for installing pg library

---
+++ src/db_connect.js
@@ -0,0 +1,27 @@
+const Pool = require("pg").Pool;
+//access dictionary value for Pool key
+//in the pg library
+
+//can install pg library with the following command:
+// $ npm install --save pg
+
+//pg library is a library that we installed
+//we use require("<library name>") to get the information from
+//the library
+//in this case we are pulling the Pool object?
+
+const pool = new Pool({
+  user: "postgres", //username for postgres database
+  password: "kthl8822", //password for postgres database
+
+  host: "localhost", //our host
+  port: 5432, //port of the host
+
+
+  database: "meditrack_tb" //the database we will be working wit
+});
+
+//export the pool
+Object.assign(module.exports, {
+    pool
+  });
