f3c50fcd68164d59da30091eb9439420e62625da
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2021-03-01 20:39:11 -0500

    Database Set Up
    This is an sql file for all the commands you run to set up our database, im still working on it

---
+++ src/db_setup_commands.sql
@@ -0,0 +1,8 @@
+-- you copy and paste the following commands in the postgres command line
+-- still workin on it
+CREATE DATABASE meditrack_db;
+
+CREATE TABLE prescriptions(
+	RX PRIMARY KEY;
+	UserID 
+)
