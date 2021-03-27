32672f9bcda30df2528f0fb9b0583b0a68b98679
Jamie Won <jamie.won.2000@gmail.com>
2021-03-27 17:39:57 -0400

    basic styling and linking pages for patient view

--- prescription-tracker-client/src/App.js
+++ prescription-tracker-client/src/App.js
@@ -3,6 +3,10 @@ import { Switch, Route } from "react-router-dom";
 
 // Pages
 import { Home, Login, PageNotFound } from "./Pages";
+import Patient from './Pages/Patient/Patient';
+import History from './Pages/Patient/History';
+import Contact from './Pages/Patient/Contact';
+import Prescription from './Pages/Patient/Prescription';
 
 function App() {
   return (
@@ -18,6 +22,30 @@ function App() {
           exact path="/Login"
           render={props => 
             <Login {...props} />
+          }
+        />
+        <Route 
+          exact path="/Patient"
+          render={props => 
+            <Patient {...props} />
+          }
+        />
+        <Route 
+          exact path="/Patient/Contact"
+          render={props => 
+            <Contact {...props} />
+          }
+        />
+        <Route 
+          exact path="/Patient/Prescription"
+          render={props => 
+            <Prescription {...props} />
+          }
+        />
+        <Route 
+          exact path="/Patient/History"
+          render={props => 
+            <History {...props} />
           }
         />
         <Route
