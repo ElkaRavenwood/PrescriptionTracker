4c7907adc8d04cd8c3790dc7b6ffe5defe733cf5
Jamie Won <jamie.won.2000@gmail.com>
2021-03-30 11:52:19 -0400

    Non required healthcard

--- prescription-tracker-client/src/Pages/Home/Signup.js
+++ prescription-tracker-client/src/Pages/Home/Signup.js
@@ -58,65 +58,73 @@ const Signup = (props) => {
     }
 
     const handleSubmit = () => {
+        // checks required fields
+        if (state.firstName=== "" && state.lastName=== "" && state.email=== "jane.doe@email.ca" && state.phone=== "" && state.streetAddress=== "" && state.city=== "" && state.postalCode=== "" && state.securityQ1=== "" && state.securityA1=== "" && state.securityQ2=== "" && state.securityA2=== "" && state.securityQ3=== "" && state.securityA3=== "" && state.password=== "" && state.confirmPassword=== "") {
+            setState((state) => ({...state, error: true}))
+        } else {
         // TODO Ping backend
+
+        }
     }
 
+
+    // TODO change all security questions to dropdowns
     return ( 
         <Grid container className={classes.root} spacing={3}>
             <Grid xs={12} item><Typography variant="h1">Sign Up</Typography></Grid>
             <Grid xs={12} item><Typography variant="h2">General Information</Typography></Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="First Name" variant="filled" value={state.firstName} name="firstName" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="First Name" variant="filled" value={state.firstName} name="firstName" onChange={handleText} required fullWidth error={state.error && state.firstName ===""}/>
             </Grid>
             <Grid xs={4} item>
                 <TextField className={classes.text_element} label="Middle Name" variant="filled" value={state.middleName} name="middleName" onChange={handleText} fullWidth/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Last Name" variant="filled" value={state.lastName} name="lastName" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Last Name" variant="filled" value={state.lastName} name="lastName" onChange={handleText} required fullWidth error={state.error && state.lastName ===""}/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Email Adress" variant="filled" value={state.email} name="email" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Email Adress" variant="filled" value={state.email} name="email" onChange={handleText} required fullWidth error={state.error && state.email ===""}/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Phone Number" variant="filled" value={state.phone} name="phone" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Phone Number" variant="filled" value={state.phone} name="phone" onChange={handleText} required fullWidth error={state.error && state.phone ===""}/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Health Card Number" variant="filled" value={state.healthCard} name="healthCard" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Health Card Number" variant="filled" value={state.healthCard} name="healthCard" onChange={handleText} fullWidth/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Street Adress" variant="filled" value={state.streetAddress} name="streetAddress" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Street Adress" variant="filled" value={state.streetAddress} name="streetAddress" onChange={handleText} required fullWidth error={state.error && state.streetAddress ===""}/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="City" variant="filled" value={state.city} name="city" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="City" variant="filled" value={state.city} name="city" onChange={handleText} required fullWidth error={state.error && state.city ===""}/>
             </Grid>
             <Grid xs={4} item>
-                <TextField className={classes.text_element} label="Postal Code" variant="filled" value={state.postalCode} name="postalCode" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Postal Code" variant="filled" value={state.postalCode} name="postalCode" onChange={handleText} required fullWidth error={state.error && state.postalCode ===""}/>
             </Grid>
 
             <Grid xs={12} item><Typography variant="h2">Security Questions</Typography></Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Security Question #1" variant="filled" value={state.securityQ1} name="securityQ1" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Security Question #1" variant="filled" value={state.securityQ1} name="securityQ1" onChange={handleText} required fullWidth error={state.error && state.securityQ1 ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Answer #1" variant="filled" value={state.securityA1} name="securityA1" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Answer #1" variant="filled" value={state.securityA1} name="securityA1" onChange={handleText} required fullWidth error={state.error && state.securityA1 ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Security Question #2" variant="filled" value={state.securityQ2} name="securityQ2" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Security Question #2" variant="filled" value={state.securityQ2} name="securityQ2" onChange={handleText} required fullWidth error={state.error && state.securityQ2 ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Answer #2" variant="filled" value={state.securityA2} name="securityA2" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Answer #2" variant="filled" value={state.securityA2} name="securityA2" onChange={handleText} required fullWidth error={state.error && state.securityA2 ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Security Question #3" variant="filled" value={state.securityQ3} name="securityQ3" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Security Question #3" variant="filled" value={state.securityQ3} name="securityQ3" onChange={handleText} required fullWidth error={state.error && state.securityQ3 ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Answer #3" variant="filled" value={state.securityA3} name="securityA3" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Answer #3" variant="filled" value={state.securityA3} name="securityA3" onChange={handleText} required fullWidth error={state.error && state.securityA3===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Password" variant="filled" value={state.password} name="password" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Password" variant="filled" value={state.password} name="password" onChange={handleText} required fullWidth error={state.error && state.password ===""}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField className={classes.text_element} label="Confirm Password" variant="filled" value={state.confirmPassword} name="confirmPassword" onChange={handleText} required fullWidth/>
+                <TextField className={classes.text_element} label="Confirm Password" variant="filled" value={state.confirmPassword} name="confirmPassword" onChange={handleText} required fullWidth error={state.error && state.confirmPassword ===""}/>
             </Grid>
             <Grid item xs={12}>
                 <Button className={classes.button} variant="outlined" onClick={handleSubmit}>Sign Up</Button>
