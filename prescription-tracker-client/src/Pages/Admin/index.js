849017394719d818b4cf2d2bd2d7d278985e50ce
Jamie Won <jamie.won.2000@gmail.com>
2021-03-30 15:03:00 -0400

    adding page that shows prescription added

--- prescription-tracker-client/src/Pages/Admin/index.js
+++ prescription-tracker-client/src/Pages/Admin/index.js
@@ -1,3 +1,4 @@
 export { default as Admin } from "./Admin.js";
 export { default as PatientDetails } from "./PatientDetails.js";
 export { default as AddPrescription } from "./AddPrescription.js";
+export { default as PrescriptionSuccess } from "./PrescriptionSuccess.js";
